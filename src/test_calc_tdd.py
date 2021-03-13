ab02b38e37eaa4294428f5e56c140fe04dd47486
raspbeep <pavel4000@gmail.com>
2021-03-13 19:39:10 +0100

    multiplication, subtraction tests, DivByZero Exception

--- src/test_calc_tdd.py
+++ src/test_calc_tdd.py
@@ -1,17 +1,18 @@
-#TODO HLAVICKA
-
+# TODO HLAVICKA
 
 
 import unittest
 import calc
 
+
 ## Documentation for a class.
 #
-#  More details.
+#  Unit Test for basic mathematical operation e.g. addition
+#  subtraction, multiplication, division.
 
 
-class TestCalc(unittest.TestCase):
-    ## Documentation for a method.
+class TestBasicOperations(unittest.TestCase):
+    ## Set of tests for addition
     #  @param self The object pointer.
     def test_add(self):
         result_pos_operands = calc.math_lib.add(7, 7)
@@ -32,6 +33,8 @@ class TestCalc(unittest.TestCase):
         result_neg_zero = calc.math_lib.add(-42, 0)
         self.assertEqual(result_neg_zero, -42)
 
+    ## Set of tests for subtraction
+    #  @param self The object pointer.
     def test_sub(self):
         result_pos_operands_1 = calc.math_lib.sub(2, 7)
         self.assertEqual(result_pos_operands_1, -5)
@@ -50,3 +53,49 @@ class TestCalc(unittest.TestCase):
 
         result_neg_zero = calc.math_lib.sub(-33, 0)
         self.assertEqual(result_neg_zero, -33)
+
+    ## Set of tests for multiplication
+    #  @param self The object pointer.
+    def test_mult(self):
+        result_pos_operands = calc.math_lib.mult(2, 7)
+        self.assertEqual(result_pos_operands, 14)
+
+        result_neg_operands = calc.math_lib.mult(-2, -14)
+        self.assertEqual(result_neg_operands, 28)
+
+        result_pos_neg_operands_1 = calc.math_lib.mult(10, -5)
+        self.assertEqual(result_pos_neg_operands_1, -50)
+
+        result_pos_neg_operands_2 = calc.math_lib.mult(-15, 3)
+        self.assertEqual(result_pos_neg_operands_2, -45)
+
+        result_zero = calc.math_lib.mult(33, 0)
+        self.assertEqual(result_zero, 0)
+
+        result_neg_zero = calc.math_lib.mult(-33, 0)
+        self.assertEqual(result_neg_zero, 0)
+
+    ## Set of tests for division
+    #  @param self The object pointer.
+    def test_div(self):
+        result_pos_operands = calc.math_lib.div(6, 3)
+        self.assertEqual(result_pos_operands, 2)
+
+        result_neg_operands = calc.math_lib.div(-14, -2)
+        self.assertEqual(result_neg_operands, 7)
+
+        result_pos_neg_operands_1 = calc.math_lib.div(10, -5)
+        self.assertEqual(result_pos_neg_operands_1, -2)
+
+        result_pos_neg_operands_2 = calc.math_lib.div(-15, 3)
+        self.assertEqual(result_pos_neg_operands_2, -5)
+
+        result_pos_zero = calc.math_lib.div(0, 33)
+        self.assertEqual(result_pos_zero, 0)
+
+        result_neg_zero = calc.math_lib.div(0, -33)
+        self.assertEqual(result_neg_zero, 0)
+
+        with self.assertRaises(calc.math_lib.DivByZero):
+            calc.math_lib.div(33, 0)
+            calc.math_lib.div(-33, 0)
