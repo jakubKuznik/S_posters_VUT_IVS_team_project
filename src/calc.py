5baf826258a80b0ae382cafdc0b09e89238338e4
shadov <kuznja16@gmail.com>
2021-03-25 12:15:06 +0100

    Creating first gui.

--- src/calc.py
+++ src/calc.py
@@ -21,19 +21,37 @@
 #  @author Kuzník Jakub
 #  @author Kratochvíl Pavel
     #  @author Svobodová Lucie
-
+import sys
 import time
 # Mathematical library of all operation and Exceptions
 import math_lib
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from gui import *
+
+
+##
+# Function ll create window that was create
+# button click meaning
+# n == number_button (0 1 2 3)
+# s == symbol button (div, mult, percent ....)
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_s_cals()
+        self.ui.setupUi(self)
+        self.show()
 
 
 ## Documentation for a function.
 #  This is how doxygen function documentation looks like .
 def main():
 
-    ## THIS COMMENT WILL BE IN DOXYGEN
-    print(math_lib.fact(-5))
-    # THIS COMENT WILL NOT BE IN DOXYGEN
+    calc = QApplication(sys.argv)
+    instance = Main()
+    sys.exit(calc.exec_())
 
     #while true(do MATH)
         #read numbers | operation
