adf3cb105cc9d741f5c058c3f0c21cf9664a99c6
Headclass <michalvano000@gmail.com>
2021-03-31 00:56:29 +0200

    Experimental method of latex rendering

--- src/calc.py
+++ src/calc.py
@@ -1,60 +1,80 @@
-###########################################
-# Project name: IVS - projekt
-# File: calc.py
-# Date: 13. 03. 2021
-# Last change: 13. 03. 2021
-# Team: S_POSTERS
-# Supervisor: ...
-# Authors:  Vaňo Michal
-#           Kuzník Jakub
-#           Kratochvíl Pavel
-#           Svobodová Lucie
-#
-# Brief: Calculator
-###########################################
-
-## @file calc.py
-#
-#  @brief calculator
-#
-#  @author Vaňo Michal
-#  @author Kuzník Jakub
-#  @author Kratochvíl Pavel
-#  @author Svobodová Lucie
-import sys
-import time
-# Mathematical library of all operation and Exceptions
-import math_lib
-
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 from gui import *
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>MathJax v3 with interactive TeX input and HTML output</title>
+  <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"></script>
+  <script>
+    function convert(input) {
+      output = document.getElementById('output');
+      output.innerHTML = '';
+      MathJax.texReset();
+      var options = MathJax.getMetricsFor(output);
+      options.display = true;
+      MathJax.tex2chtmlPromise(input, options).then(function (node) {
+        output.appendChild(node);
+        MathJax.startup.document.clear();
+        MathJax.startup.document.updateDocument();
+      }).catch(function (err) {
+        output.appendChild(document.createTextNode(err.message));
+      });
+    }
+  </script>
+  <style>
+  body, html {
+    padding: 0;
+    margin: 0;
+  }
+  #output {
+    font-size: 120%;
+    min-height: 2em;
+    padding: 0;
+    margin: 0;
+  }
+  </style>
+</head>
+<body>
+<div id="output"></div>
+</body>
+</html>
+"""
 
 
-##
-# Function ll create window that was create
-# button click meaning
-# n == number_button (0 1 2 3)
-# s == symbol button (div, mult, percent ....)
-class Main(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_s_cals()
-        self.ui.setupUi(self)
-        self.show()
+class Window(QtWidgets.QWidget):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        ui = Window_Ui()
+        ui.setupUi(self)
+        self._ui = ui
+        ui.view.setHtml(html)
+        page = ui.view.page()
+        page.loadFinished.connect(self.onLoadFinished)
+        ui.edit.setText("{-b \\pm \\sqrt{b^2-4ac} \\over 2a}")
+        ui.edit.textChanged.connect(self.onTextChanged)
+        self._ready = False
+
+    def onLoadFinished(self):
+        if self._ready:
+            return
+        self._ready = True
+        self.onTextChanged(self._ui.edit.text())
+
+    def onTextChanged(self, text):
+        ui = self._ui
+        text = text.replace("\\","\\\\")
+        page = ui.view.page()
+        page.runJavaScript('convert("{}");'.format(text))
+
+    def sizeHint(self):
+        return QtCore.QSize(300,150)
 
 
-## Documentation for a function.
-#  This is how doxygen function documentation looks like .
-def main():
-    calc = QApplication(sys.argv)
-    instance = Main()
-    sys.exit(calc.exec_())
-
-
-#This is hou the main function is defined in python
-if __name__ == '__main__':
-    start = time.time()
-    main()
-    print(time.time() - start)
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = Window()
+    window.show()
+    app.exec()
