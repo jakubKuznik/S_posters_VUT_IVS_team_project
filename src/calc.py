32e434c45192acda1161535b70bd184cbd71b9c5
raspbeep <pavel4000@gmail.com>
2021-04-15 20:12:30 +0200

    Erased redundat pyqt5 MainWindows class. Prettified, added fixed sized command, moved button declarations to init, erased useless names for each object, removed redundant imports, removed useless imports

--- src/calc.py
+++ src/calc.py
@@ -21,40 +21,19 @@
 #  @author Kuzník Jakub
 #  @author Kratochvíl Pavel
 #  @author Svobodová Lucie
-import sys
-import time
-# Mathematical library of all operation and Exceptions
-import math_lib
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+import sys
+
 from PyQt5.QtWidgets import *
 from gui import *
-
-
-##
-# Function ll create window that was create
-# button click meaning
-# n == number_button (0 1 2 3)
-# s == symbol button (div, mult, percent ....)
-class Main(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_s_cals()
-        self.ui.setupUi(self)
-        self.show()
-
-
 ## Documentation for a function.
 #  This is how doxygen function documentation looks like .
 def main():
-    calc = QApplication(sys.argv)
-    instance = Main()
+    calc = QtWidgets.QApplication(sys.argv)
+    app = App()
+    app.show()
     sys.exit(calc.exec_())
 
 
-#This is hou the main function is defined in python
 if __name__ == '__main__':
-    start = time.time()
     main()
-    print(time.time() - start)
