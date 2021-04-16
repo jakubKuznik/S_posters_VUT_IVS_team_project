aeff5cac91747abfba2e18842c9e1542d88c709b
raspbeep <pavel4000@gmail.com>
2021-04-16 11:38:00 +0200

    Digit buttons refactor

--- src/gui.py
+++ src/gui.py
@@ -12,20 +12,11 @@ class App(QWidget):
         self.result = 0
         self.operators = ['+', '-', '*', '/', '**0.5', '.']
         self.numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-        self.setFixedSize(290, 500)
+        self.setFixedSize(290, 480)
         # TODO: icon
 
         # UI ELEMENTS
-        self.pushButton_n9 = QPushButton(self)
-        self.pushButton_n8 = QPushButton(self)
-        self.pushButton_n7 = QPushButton(self)
-        self.pushButton_n6 = QPushButton(self)
-        self.pushButton_n5 = QPushButton(self)
-        self.pushButton_n4 = QPushButton(self)
-        self.pushButton_n3 = QPushButton(self)
-        self.pushButton_n2 = QPushButton(self)
-        self.pushButton_n1 = QPushButton(self)
-        self.pushButton_n0 = QPushButton(self)
+
         self.pushButton_smem = QPushButton(self)
         self.pushButton_sdot = QPushButton(self)
         self.pushButton_s_equal = QPushButton(self)
@@ -43,6 +34,19 @@ class App(QWidget):
         self.pushButton_help = QtWidgets.QPushButton(self)
         self.output1 = QtWidgets.QLabel(self)
         self.output2 = QtWidgets.QLabel(self)
+
+        self.list_of_buttons = []
+
+        self.digit_buttons = [["0", [80, 400, 61, 61]],
+                              ["1", [10, 330, 61, 61]],
+                              ["2", [80, 330, 61, 61]],
+                              ["3", [150, 330, 61, 61]],
+                              ["4", [10, 260, 61, 61]],
+                              ["5", [80, 260, 61, 61]],
+                              ["6", [150, 260, 61, 61]],
+                              ["7", [10, 190, 61, 61]],
+                              ["8", [80, 190, 61, 61]],
+                              ["9", [150, 190, 61, 61]]]
 
         self.setup_ui()
 
@@ -63,90 +67,55 @@ class App(QWidget):
         self.resize(300, 500)
         self.setWindowTitle("Imposter Calculator")
 
-        self.pushButton_n9.setGeometry(QtCore.QRect(150, 190, 61, 61))
-        self.pushButton_n9.setText("9")
-
-        self.pushButton_n8.setGeometry(QtCore.QRect(80, 190, 61, 61))
-        self.pushButton_n8.setText("8")
-
-        self.pushButton_n7.setGeometry(QtCore.QRect(10, 190, 61, 61))
-        self.pushButton_n7.setText("7")
-
-        self.pushButton_n6.setGeometry(QtCore.QRect(150, 260, 61, 61))
-        self.pushButton_n6.setText("6")
-
-        self.pushButton_n5.setGeometry(QtCore.QRect(80, 260, 61, 61))
-        self.pushButton_n5.setText("5")
-
-        self.pushButton_n4.setGeometry(QtCore.QRect(10, 260, 61, 61))
-        self.pushButton_n4.setText("4")
-
-        self.pushButton_n3.setGeometry(QtCore.QRect(150, 330, 61, 61))
-        self.pushButton_n3.setText("3")
-
-        self.pushButton_n2.setGeometry(QtCore.QRect(80, 330, 61, 61))
-        self.pushButton_n2.setText("2")
-
-        self.pushButton_n1.setGeometry(QtCore.QRect(10, 330, 61, 61))
-        self.pushButton_n1.setText("1")
-
-        self.pushButton_n0.setGeometry(QtCore.QRect(80, 400, 61, 61))
-        self.pushButton_n0.setText("0")
+        for i in range(10):
+            x_coord = self.digit_buttons[i][1][0]
+            y_coord = self.digit_buttons[i][1][1]
+            text = self.digit_buttons[i][0]
+            size = 61
+            button = QPushButton(self.digit_buttons[i][0], self)
+            button.setGeometry(QtCore.QRect(x_coord, y_coord, size, size))
+            button.clicked.connect(lambda: self.print(text, text))
 
         self.pushButton_smem.setGeometry(QtCore.QRect(10, 400, 61, 61))
-        self.pushButton_smem.setAutoFillBackground(False)
         self.pushButton_smem.setText("M")
 
         self.pushButton_sdot.setGeometry(QtCore.QRect(150, 400, 61, 61))
-        self.pushButton_sdot.setAutoFillBackground(False)
         self.pushButton_sdot.setText(",")
 
         self.pushButton_s_equal.setGeometry(QtCore.QRect(220, 400, 61, 61))
-        self.pushButton_s_equal.setAutoFillBackground(False)
         self.pushButton_s_equal.setText("=")
 
         self.pushButton_smultiply.setGeometry(QtCore.QRect(220, 190, 61, 61))
-        self.pushButton_smultiply.setAutoFillBackground(False)
         self.pushButton_smultiply.setText("*")
 
         self.pushButton_sminus.setGeometry(QtCore.QRect(220, 260, 61, 61))
-        self.pushButton_sminus.setAutoFillBackground(False)
         self.pushButton_sminus.setText("-")
 
         self.pushButton_splus.setGeometry(QtCore.QRect(220, 330, 61, 61))
-        self.pushButton_splus.setAutoFillBackground(False)
         self.pushButton_splus.setText("+")
 
         self.pushButton_left_bracket.setGeometry(QtCore.QRect(10, 140, 61, 41))
-        self.pushButton_left_bracket.setAutoFillBackground(False)
         self.pushButton_left_bracket.setText("(")
 
         self.pushButton_right_bracket.setGeometry(QtCore.QRect(80, 140, 61, 41))
-        self.pushButton_right_bracket.setAutoFillBackground(False)
         self.pushButton_right_bracket.setText(")")
 
         self.pushButton_back.setGeometry(QtCore.QRect(150, 140, 61, 41))
-        self.pushButton_back.setAutoFillBackground(False)
         self.pushButton_back.setText("←")
 
         self.pushButton_sdivide.setGeometry(QtCore.QRect(220, 140, 61, 41))
-        self.pushButton_sdivide.setAutoFillBackground(False)
         self.pushButton_sdivide.setText("÷")
 
         self.pushButton_smemminus.setGeometry(QtCore.QRect(220, 90, 61, 41))
-        self.pushButton_smemminus.setAutoFillBackground(False)
         self.pushButton_smemminus.setText("M-")
 
         self.pushButton_sqrt.setGeometry(QtCore.QRect(80, 90, 61, 41))
-        self.pushButton_sqrt.setAutoFillBackground(False)
         self.pushButton_sqrt.setText("[2]√")
 
         self.pushButton_ssqr.setGeometry(QtCore.QRect(10, 90, 61, 41))
-        self.pushButton_ssqr.setAutoFillBackground(False)
         self.pushButton_ssqr.setText("^")
 
         self.pushButton_smemplus.setGeometry(QtCore.QRect(150, 90, 61, 41))
-        self.pushButton_smemplus.setAutoFillBackground(False)
         self.pushButton_smemplus.setText("M+")
 
         self.pushButton_help.setGeometry(QtCore.QRect(0, 0, 35, 35))
@@ -159,16 +128,6 @@ class App(QWidget):
         self.output2.setGeometry(QtCore.QRect(20, 41, 251, 71))
         self.output2.setFont(QFont('Comic Sans MS', 20))
 
-        self.pushButton_n0.clicked.connect(lambda: self.print("0", "0"))
-        self.pushButton_n1.clicked.connect(lambda: self.print("1", "1"))
-        self.pushButton_n2.clicked.connect(lambda: self.print("2", "2"))
-        self.pushButton_n3.clicked.connect(lambda: self.print("3", "3"))
-        self.pushButton_n4.clicked.connect(lambda: self.print("4", "4"))
-        self.pushButton_n5.clicked.connect(lambda: self.print("5", "5"))
-        self.pushButton_n6.clicked.connect(lambda: self.print("6", "6"))
-        self.pushButton_n7.clicked.connect(lambda: self.print("7", "7"))
-        self.pushButton_n8.clicked.connect(lambda: self.print("8", "8"))
-        self.pushButton_n9.clicked.connect(lambda: self.print("9", "9"))
         self.pushButton_back.clicked.connect(self.delete)
         self.pushButton_splus.clicked.connect(lambda: self.print("+", "+"))
         self.pushButton_sminus.clicked.connect(lambda: self.print("-", "-"))
@@ -184,7 +143,6 @@ class App(QWidget):
         self.pushButton_right_bracket.clicked.connect(lambda: self.print(')', ')'))
         self.output1.setText(''.join(self.displayed_content))
         self.pushButton_help.clicked.connect(self.show_dialog)
-
 
     def change(self, command):
         self.content.append(command)
