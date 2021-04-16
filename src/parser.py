ac6b0aeb78b006891b34694413a11de08bd6e873
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-04-16 20:51:23 +0200

    header changed

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,28 @@
-## This function will turn the string into the reverse polish notation
+###########################################
+# Project name: IVS - projekt
+# File: parser.py
+# Date: 14. 03. 2021
+# Last change: 15. 04. 2021
+# Team: S_POSTERS
+# Supervisor: ...
+# Authors:  Vaňo Michal
+#           Kuzník Jakub
+#           Kratochvíl Pavel
+#           Svobodová Lucie
+#
+# Brief: Parser
+###########################################
 
+## @file parser.py
+#
+#  @brief parser
+#
+#  @author Vaňo Michal
+#  @author Kuzník Jakub
+#  @author Kratochvíl Pavel
+#  @author Svobodová Lucie
+
+## This function will turn the string into the reverse polish notation
 input = "3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2 ^ 3"
 input = input.split(' ')
 
