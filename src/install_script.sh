7a7e89a24392ed9efbfa815be3806618af766e4e
raspbeep <pavel4000@gmail.com>
2021-04-17 13:32:47 +0200

    Changes to makefile ans install_script.sh to package all necessary files.

--- src/install_script.sh
+++ src/install_script.sh
@@ -1,7 +1,12 @@
 mkdir -p ../installer/usr/share/impcalc 2>/dev/null
-cp  math_lib.py ../installer/usr/share/impcalc/math_lib.py
-cp  gui.py ../installer/usr/share/impcalc/gui.py
+
+cp math_lib.py ../installer/usr/share/impcalc/math_lib.py
+cp gui.py ../installer/usr/share/impcalc/gui.py
+cp Quicksand.ttf ../installer/usr/share/impcalc/Quicksand.ttf
+cp parser.py ../installer/usr/share/impcalc/parser.py
+cp help_form.html ../installer/usr/share/impcalc/help_form.html
 cp  calc.py ../installer/usr/share/impcalc/calc.py && chmod +x ../installer/usr/share/impcalc/calc.py
+
 mkdir -p ../installer/usr/local/bin 2>/dev/null
 ln -sf /usr/share/impcalc/calc.py ../installer/usr/local/bin/impcalc-calculator
 mkdir ../installer/tmp 2>/dev/null
