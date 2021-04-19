a2e36ac82eb52d42dd8249c6e2d274f94f7a0ea1
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-04-20 00:46:50 +0200

    comments and modulo exception added

--- src/math_lib.py
+++ src/math_lib.py
@@ -2,7 +2,7 @@
 # Project name: IVS - projekt
 # File: math_lib.py
 # Date: 13. 03. 2021
-# Last change: 13. 03. 2021
+# Last change: 20. 04. 2021
 # Team: S_POSTERS
 # Supervisor: ...
 # Authors:  Vaňo Michal
@@ -13,14 +13,16 @@
 # Brief: Math library
 ###########################################
 
-## @file math_lib.py
+## 
+# @package math_lib
+# @file math_lib.py
 #
-#  @brief math library
+# @brief math library
 #
-#  @author Vaňo Michal
-#  @author Kuzník Jakub
-#  @author Kratochvíl Pavel
-#  @author Svobodová Lucie
+# @author Vaňo Michal
+# @author Kuzník Jakub
+# @author Kratochvíl Pavel
+# @author Svobodová Lucie
 
 class DivByZero(Exception):
     pass
@@ -30,72 +32,98 @@ class DomainError(Exception):
     pass
 
 
-## @brief This function will return the result of addition of two numbers.
+## 
+# @brief Function returns the result of addition of two numbers.
 #
-#  @param a first operand
-#  @param b second operand
-#  @return a+b
+# @param a first summand
+# @param b second summand
+# @return sum of two numbers
 def add(a, b):
     return a+b
 
 
-## This function will return the result of subtraction of two numbers.
-#  @param a first operand
-#  @param b second operand
+## 
+# @brief This function will return the result of subtraction of two numbers.
+#
+# @param a first operand
+# @param b second operand
+# @return difference of two numbers
 def sub(a, b):
     return a-b
 
 
-## This function will return the result of multiplication of two numbers.
-#  @param a first operand
-#  @param b second operand
+## 
+# @brief This function will return the result of multiplication of two numbers.
+# 
+# @param a first factor
+# @param b second factor
+# @return product of two numbers
 def mult(a, b):
     return a*b
 
 
-## This function will return the result of division between two numbers.
-#  This is how doxygen function documentation looks like.
-#  @param a first operand
-#  @param b second operand
+## 
+# @brief This function will return the result of division between two numbers.
+# 
+# @param a dividend
+# @param b divisor
+# @exception DivByZero in case the divisor is equal to zero
+# @return quotient of two numbers
 def div(a, b):
     if b == 0:
         raise DivByZero
     return a/b
 
 
-## This function will return the b-th root of a.
-#  @param a first operand
-#  @param b second operand
+## 
+# @brief Function will return the b-th root of a.
+#
+# @param a the base
+# @param b the degree
+# @exception DomainError when b is negative value
+# @return b-th root of a
 def root(a, b):
     if b < 0:
         raise DomainError
     return a**(1/b)
 
 
-## This function will return the factorial of a given positive integer..
-#  @param factorized_number 
-def fact(factorized_number):
-    if type(factorized_number) != int or factorized_number < 0:
+## 
+# @brief This function will return the factorial of a given positive integer.
+#
+# @param factorizedNumber factorial of this number will be calculated
+# @exception DomainError in case factorizedNumber is not integer or is negative
+# @return factorial of number factorizedNumber
+def fact(factorizedNumber):
+    if type(factorizedNumber) != int or factorizedNumber < 0:
         raise DomainError
-    if factorized_number == 0:
+    if factorizedNumber == 0:
         return 1
     else:
-        return factorized_number * fact(factorized_number - 1)
+        return factorizedNumber * fact(factorizedNumber - 1)
 
-## This is function for sqr operation
+## 
+# @brief This function will return the result of exponentiation.
 #
-#
-def exponentiation(base, power):
-    if (power == 0 and base == 0):
+# @param base base of the power
+# @param exponent power value
+# @exception DomainError when exponent and base are both equal to zero
+# @return base raised to the power of exponent
+def exponentiation(base, exponent):
+    if (exponent == 0 and base == 0):
         raise DomainError
-    return base ** power
+    return base ** exponent
 
 
-## @brief This is modulo % operation function
+## 
+# @brief This function will return the remainder of division between two numbers.
 #
-#  @param a first operand
-#  @param b second operand
-#  @return
+# @param a dividend
+# @param b divisor
+# @exception DivByZero when the divisor is equal to zero
+# @return remainder of a divided by b
 def modulo(a, b):
+    if b == 0:
+        raise DivByZero
     return a % b
 
